eba92341c82e290dfc11ef89a2cc28f40d099ee1
Henrik Lehtinen <henrik.lehtinen@gmail.com>
2017-03-07 19:29:33 +0200

    Simplex noise: swapped permutation table lookups with hash function calls, 10x speedup. Some things left to squeeze.

--- cudaNoise/cudaNoise.cu
+++ cudaNoise/cudaNoise.cu
@@ -141,6 +141,16 @@ __device__  float dot(float3 g, float x, float y, float z) {
 	return g.x*x + g.y*y + g.z*z;
 }
 
+__device__ short int calcPerm(int p)
+{
+	return (short int)(hash(p) % 256);
+}
+
+__device__ short int calcPerm12(int p)
+{
+	return (short int)(hash(p) % 12);
+}
+
 // Simplex noise adapted from Java code by Stefan Gustafson and Peter Eastman
 // SLOW AND EXPERIMENTAL
 __device__ float simplexNoise(float3 pos, float scale, int seed)
@@ -152,30 +162,6 @@ __device__ float simplexNoise(float3 pos, float scale, int seed)
 	float3 grad3[] = { make_float3(1,1,0), make_float3(-1,1,0), make_float3(1,-1,0), make_float3(-1,-1,0),
 		make_float3(1,0,1), make_float3(-1,0,1), make_float3(1,0,-1), make_float3(-1,0,-1),
 		make_float3(0,1,1), make_float3(0,-1,1), make_float3(0,1,-1), make_float3(0,-1,-1) };
-
-	short int p[] = { 151,160,137,91,90,15,
-		131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-		190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-		88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-		77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-		102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-		135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-		5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-		223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-		129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-		251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-		49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-		138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180 };
-
-	// To remove the need for index wrapping, double the permutation table length
-	short int perm[512];
-	short int permMod12[512];
-
-	for (int i = 0; i<512; i++)
-	{
-		perm[i] = p[i & 255];
-		permMod12[i] = (short)(perm[i] % 12);
-	}
 
 	// Skewing and unskewing factors for 2, 3, and 4 dimensions
 	float F3 = 1.0 / 3.0;
@@ -232,10 +218,14 @@ __device__ float simplexNoise(float3 pos, float scale, int seed)
 	int ii = i & 255;
 	int jj = j & 255;
 	int kk = k & 255;
-	int gi0 = permMod12[ii + perm[jj + perm[kk]]];
-	int gi1 = permMod12[ii + i1 + perm[jj + j1 + perm[kk + k1]]];
-	int gi2 = permMod12[ii + i2 + perm[jj + j2 + perm[kk + k2]]];
-	int gi3 = permMod12[ii + 1 + perm[jj + 1 + perm[kk + 1]]];
+//	int gi0 = permMod12[ii + perm[jj + perm[kk]]];
+//	int gi1 = permMod12[ii + i1 + perm[jj + j1 + perm[kk + k1]]];
+//	int gi2 = permMod12[ii + i2 + perm[jj + j2 + perm[kk + k2]]];
+//	int gi3 = permMod12[ii + 1 + perm[jj + 1 + perm[kk + 1]]];
+	int gi0 = calcPerm12(ii + calcPerm(jj + calcPerm(kk)));
+	int gi1 = calcPerm12(ii + i1 + calcPerm(jj + j1 + calcPerm(kk + k1)));
+	int gi2 = calcPerm12(ii + i2 + calcPerm(jj + j2 + calcPerm(kk + k2)));
+	int gi3 = calcPerm12(ii + 1 + calcPerm(jj + 1 + calcPerm(kk + 1)));
 
 	// Calculate the contribution from the four corners
 	float t0 = 0.6 - x0*x0 - y0*y0 - z0*z0;
