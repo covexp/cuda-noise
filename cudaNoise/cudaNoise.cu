f6bbc215a068c2739ad755f38452391fa7c17c3b
Henrik Lehtinen <henrik.lehtinen@gmail.com>
2017-03-11 23:28:23 +0200

    Floating point optimizations to Simplex noise, 10-15% performance increase

--- cudaNoise/cudaNoise.cu
+++ cudaNoise/cudaNoise.cu
@@ -174,7 +174,6 @@ __device__ __constant__ short int perm[512] = { 151,160,137,91,90,15,
 
 __device__ short int calcPerm12(int p)
 {
-	//	return (short int)(hash(p) % 12);
 	return perm[p] % 12;
 }
 
@@ -191,13 +190,9 @@ __device__ float simplexNoise(float3 pos, float scale, int seed)
 	float yin = pos.y * scale;
 	float zin = pos.z * scale;
 
-/*	float3 grad3[] = { make_float3(1,1,0), make_float3(-1,1,0), make_float3(1,-1,0), make_float3(-1,-1,0),
-		make_float3(1,0,1), make_float3(-1,0,1), make_float3(1,0,-1), make_float3(-1,0,-1),
-		make_float3(0,1,1), make_float3(0,-1,1), make_float3(0,1,-1), make_float3(0,-1,-1) };
-		*/
 	// Skewing and unskewing factors for 3 dimensions
-	float F3 = 1.0 / 3.0;
-	float G3 = 1.0 / 6.0;
+	float F3 = 1.0f / 3.0f;
+	float G3 = 1.0f / 6.0f;
 
 	float n0, n1, n2, n3; // Noise contributions from the four corners
 
@@ -221,15 +216,15 @@ __device__ float simplexNoise(float3 pos, float scale, int seed)
 	if (x0 >= y0) {
 		if (y0 >= z0)
 		{
-			i1 = 1; j1 = 0; k1 = 0; i2 = 1; j2 = 1; k2 = 0;
+			i1 = 1.0f; j1 = 0.0f; k1 = 0.0f; i2 = 1.0f; j2 = 1.0f; k2 = 0.0f;
 		} // X Y Z order
-		else if (x0 >= z0) { i1 = 1; j1 = 0; k1 = 0; i2 = 1; j2 = 0; k2 = 1; } // X Z Y order
-		else { i1 = 0; j1 = 0; k1 = 1; i2 = 1; j2 = 0; k2 = 1; } // Z X Y order
+		else if (x0 >= z0) { i1 = 1.0f; j1 = 0.0f; k1 = 0.0f; i2 = 1.0f; j2 = 0.0f; k2 = 1.0f; } // X Z Y order
+		else { i1 = 0.0f; j1 = 0.0f; k1 = 1.0f; i2 = 1.0f; j2 = 0.0f; k2 = 1.0f; } // Z X Y order
 	}
 	else { // x0<y0
-		if (y0<z0) { i1 = 0; j1 = 0; k1 = 1; i2 = 0; j2 = 1; k2 = 1; } // Z Y X order
-		else if (x0<z0) { i1 = 0; j1 = 1; k1 = 0; i2 = 0; j2 = 1; k2 = 1; } // Y Z X order
-		else { i1 = 0; j1 = 1; k1 = 0; i2 = 1; j2 = 1; k2 = 0; } // Y X Z order
+		if (y0<z0) { i1 = 0.0f; j1 = 0.0f; k1 = 1.0f; i2 = 0.0f; j2 = 1; k2 = 1.0f; } // Z Y X order
+		else if (x0<z0) { i1 = 0.0f; j1 = 1.0f; k1 = 0.0f; i2 = 0.0f; j2 = 1.0f; k2 = 1.0f; } // Y Z X order
+		else { i1 = 0.0f; j1 = 1.0f; k1 = 0.0f; i2 = 1.0f; j2 = 1.0f; k2 = 0.0f; } // Y X Z order
 	}
 
 	// A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
@@ -239,47 +234,44 @@ __device__ float simplexNoise(float3 pos, float scale, int seed)
 	float x1 = x0 - i1 + G3; // Offsets for second corner in (x,y,z) coords
 	float y1 = y0 - j1 + G3;
 	float z1 = z0 - k1 + G3;
-	float x2 = x0 - i2 + 2.0*G3; // Offsets for third corner in (x,y,z) coords
-	float y2 = y0 - j2 + 2.0*G3;
-	float z2 = z0 - k2 + 2.0*G3;
-	float x3 = x0 - 1.0 + 3.0*G3; // Offsets for last corner in (x,y,z) coords
-	float y3 = y0 - 1.0 + 3.0*G3;
-	float z3 = z0 - 1.0 + 3.0*G3;
+	float x2 = x0 - i2 + 2.0f*G3; // Offsets for third corner in (x,y,z) coords
+	float y2 = y0 - j2 + 2.0f*G3;
+	float z2 = z0 - k2 + 2.0f*G3;
+	float x3 = x0 - 1.0f + 3.0f*G3; // Offsets for last corner in (x,y,z) coords
+	float y3 = y0 - 1.0f + 3.0f*G3;
+	float z3 = z0 - 1.0f + 3.0f*G3;
 
 	// Work out the hashed gradient indices of the four simplex corners
 	int ii = i & 255;
 	int jj = j & 255;
 	int kk = k & 255;
-//	int gi0 = calcPerm12(ii + calcPerm(jj + calcPerm(kk)));
-//	int gi1 = calcPerm12(ii + i1 + calcPerm(jj + j1 + calcPerm(kk + k1)));
-//	int gi2 = calcPerm12(ii + i2 + calcPerm(jj + j2 + calcPerm(kk + k2)));
-//	int gi3 = calcPerm12(ii + 1 + calcPerm(jj + 1 + calcPerm(kk + 1)));
+
 	int gi0 = calcPerm12(ii + perm[jj + perm[kk]]);
 	int gi1 = calcPerm12(ii + i1 + perm[jj + j1 + perm[kk + k1]]);
 	int gi2 = calcPerm12(ii + i2 + perm[jj + j2 + perm[kk + k2]]);
 	int gi3 = calcPerm12(ii + 1 + perm[jj + 1 + perm[kk + 1]]);
 
 	// Calculate the contribution from the four corners
-	float t0 = 0.6 - x0*x0 - y0*y0 - z0*z0;
-	if (t0<0) n0 = 0.0;
+	float t0 = 0.6f - x0*x0 - y0*y0 - z0*z0;
+	if (t0<0.0f) n0 = 0.0f;
 	else {
 		t0 *= t0;
 		n0 = t0 * t0 * dot(gradMap[gi0], x0, y0, z0);
 	}
-	float t1 = 0.6 - x1*x1 - y1*y1 - z1*z1;
-	if (t1<0) n1 = 0.0;
+	float t1 = 0.6f - x1*x1 - y1*y1 - z1*z1;
+	if (t1<0.0f) n1 = 0.0f;
 	else {
 		t1 *= t1;
 		n1 = t1 * t1 * dot(gradMap[gi1], x1, y1, z1);
 	}
-	float t2 = 0.6 - x2*x2 - y2*y2 - z2*z2;
-	if (t2<0) n2 = 0.0;
+	float t2 = 0.6f - x2*x2 - y2*y2 - z2*z2;
+	if (t2<0.0f) n2 = 0.0f;
 	else {
 		t2 *= t2;
 		n2 = t2 * t2 * dot(gradMap[gi2], x2, y2, z2);
 	}
-	float t3 = 0.6 - x3*x3 - y3*y3 - z3*z3;
-	if (t3<0) n3 = 0.0;
+	float t3 = 0.6f - x3*x3 - y3*y3 - z3*z3;
+	if (t3<0.0f) n3 = 0.0f;
 	else {
 		t3 *= t3;
 		n3 = t3 * t3 * dot(gradMap[gi3], x3, y3, z3);
@@ -287,7 +279,7 @@ __device__ float simplexNoise(float3 pos, float scale, int seed)
 
 	// Add contributions from each corner to get the final noise value.
 	// The result is scaled to stay just inside [-1,1]
-	return 32.0*(n0 + n1 + n2 + n3);
+	return 32.0f*(n0 + n1 + n2 + n3);
 }
 
 __device__ float checker(float3 pos, float scale, int seed)
